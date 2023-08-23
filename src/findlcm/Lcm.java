96df26e5daad7de50f55d3eb7892a4cc84c63f01
nasir <nasir@mislbd.com>
2023-08-23 22:34:32 +0600

    find least common multiple

---
+++ src/findlcm/Lcm.java
@@ -0,0 +1,27 @@
+package findlcm;
+
+import java.util.Scanner;
+
+public class Lcm {
+
+    private static int findLcm(int num1, int num2){
+        int lcmNumber=Math.max(num1,num2);
+        while (true){
+            if (lcmNumber%num1==0 && lcmNumber%num2==0){
+                break;
+            }
+            lcmNumber++;
+        }
+        return lcmNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please enter a number ");
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+
+        System.out.println("Least Common Multiple is: "+findLcm(num1,num2));
+
+    }
+}
